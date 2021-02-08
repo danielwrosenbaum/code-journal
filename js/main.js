6e5bf7a8cf4431d851c8192ea03aa83186dc3a2d
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-02-08 14:44:15 -0800

    Made all inputs of the journal entry form required. Added eventListener to change image URL. #1

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 /* global data */
 /* exported data */
+var changeUrl = document.querySelector('input.image');
+var photoUrl = document.querySelector('img.pic');
+
+function addUrl(event) {
+  photoUrl.src = event.target.value;
+}
+changeUrl.addEventListener('input', addUrl);
