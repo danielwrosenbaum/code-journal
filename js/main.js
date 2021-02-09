9355a08927a239b4f97d6024920d4ed3cb090ed8
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-02-08 16:53:26 -0800

    added listener events for input and started the submit events. #1

--- js/main.js
+++ js/main.js
@@ -1,9 +1,24 @@
 /* global data */
 /* exported data */
-var changeUrl = document.querySelector('input.image');
-var photoUrl = document.querySelector('img.pic');
+var $changeUrl = document.querySelector('input.image');
+var $photoUrl = document.querySelector('img.pic');
+var $formVar = document.querySelector('form');
 
 function addUrl(event) {
-  photoUrl.src = event.target.value;
+  $photoUrl.src = event.target.value;
 }
-changeUrl.addEventListener('input', addUrl);
+$changeUrl.addEventListener('input', addUrl);
+
+function theSubmit(event) {
+  var $submitObj = {};
+  $submitObj.value = event.target.value;
+  $submitObj.name = event.target.name;
+  // console.log('submit: ', $submitObj);
+  // var formName = $formVar.elements[name];
+  // console.log('form name: ', formName);
+  $submitObj = data.nextEntryId;
+  // console.log('submit: ', $submitObj);
+  data.nextEntryId++;
+
+}
+$formVar.addEventListener('submit', theSubmit);
