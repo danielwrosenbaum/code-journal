e873396d7ab64211df8d940152fcde0bbe515cd0
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-02-08 20:29:54 -0800

    got everything done except for saving in local storage correctly. #1

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@
 /* exported data */
 var $changeUrl = document.querySelector('input.image');
 var $photoUrl = document.querySelector('img.pic');
-var $formVar = document.querySelector('form');
+var $formVar = document.querySelector('.form');
+var inputValues = document.querySelectorAll('input');
 
 function addUrl(event) {
   $photoUrl.src = event.target.value;
@@ -13,12 +14,11 @@ function theSubmit(event) {
   var $submitObj = {};
   $submitObj.value = event.target.value;
   $submitObj.name = event.target.name;
-  // console.log('submit: ', $submitObj);
-  // var formName = $formVar.elements[name];
-  // console.log('form name: ', formName);
-  $submitObj = data.nextEntryId;
-  // console.log('submit: ', $submitObj);
+  $submitObj.id = data.nextEntryId;
+  localStorage.setItem($submitObj.name, $submitObj.value);
   data.nextEntryId++;
-
+  data.entries.unshift($submitObj);
+  $photoUrl.src = '';
+  inputValues.value = '';
 }
 $formVar.addEventListener('submit', theSubmit);
