e3773b614cc8b4d8215bc56197cf9ba2ef38cb94
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-02-10 13:30:07 -0800

    Add an anchor to the entries view that shows the entry form. #2

--- js/data.js
+++ js/data.js
@@ -10,10 +10,11 @@ var data = {
 var previousTodosJSON = localStorage.getItem('data');
 
 window.addEventListener('load', function () {
-  if (previousTodosJSON !== null) {
-    data = JSON.parse(previousTodosJSON);
-  }
+
 });
+if (previousTodosJSON !== null) {
+  data = JSON.parse(previousTodosJSON);
+}
 window.addEventListener('beforeunload', function () {
   var todosJSON = JSON.stringify(data);
   localStorage.setItem('data', todosJSON);
