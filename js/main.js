ad1ab0153830e05505a09d3546e0fa4d63e89ece
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-02-10 12:52:43 -0800

    Remove the example HTML entries once the user's entries are successfully being appended to the DOM on page load. #2

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 var $changeUrl = document.querySelector('input.image');
 var $photoUrl = document.querySelector('img.pic');
 var $formVar = document.querySelector('.form');
+var theList = document.querySelector('.listEntries');
 
 function addUrl(event) {
   $photoUrl.src = event.target.value;
@@ -11,6 +12,7 @@ $changeUrl.addEventListener('input', addUrl);
 
 function theSubmit(event) {
   event.preventDefault();
+  var i = 0;
   var notesVal = $formVar.elements.notes.value;
   var titleVal = $formVar.elements.title.value;
   var picVal = $formVar.elements.imageURL.value;
@@ -20,43 +22,43 @@ function theSubmit(event) {
   $submitObj.title = titleVal;
   $submitObj.id = data.nextEntryId;
   data.entries.unshift($submitObj);
+  var newListEntries = journalReturn(data.entries[i]);
+  theList.prepend(newListEntries);
   data.nextEntryId++;
   $photoUrl.src = 'images/placeholder-image-square.jpg';
   $formVar.reset();
 }
-
 $formVar.addEventListener('submit', theSubmit);
-var journalEntry = document.querySelector('.listEntries');
+
+// var journalEntry = document.querySelector('.listEntries');
 function journalReturn(data) {
   var newListItem = document.createElement('li');
   newListItem.className = 'newEntry row';
-  journalEntry.appendChild(newListItem);
+  theList.appendChild(newListItem);
   var divColumn = document.createElement('div');
   newListItem.appendChild(divColumn);
   divColumn.className = 'column-half';
   var newImage = document.createElement('img');
   divColumn.appendChild(newImage);
   newImage.className = 'entry-pic';
-  newImage.setAttribute('src', data.entries[0].imageURL);
+  newImage.setAttribute('src', data.imageURL);
   var divColumn2 = document.createElement('div');
   newListItem.appendChild(divColumn2);
   divColumn2.className = 'column-half';
   var newTitle = document.createElement('h3');
   divColumn2.appendChild(newTitle);
   newTitle.className = 'titles';
-  newTitle.textContent = data.entries[0].title;
+  newTitle.textContent = data.title;
   var newNotes = document.createElement('p');
   divColumn2.appendChild(newNotes);
   newNotes.className = 'entry-notes';
-  newNotes.textContent = data.entries[0].notes;
-  return journalEntry;
+  newNotes.textContent = data.notes;
+  return newListItem;
 }
 
-var theList = document.querySelector('.listEntries');
-$formVar.addEventListener('DOMContentLoaded', function (event) {
+window.addEventListener('DOMContentLoaded', function (event) {
   for (var i = 0; i < data.entries.length; i++) {
-    var newListEntries = journalReturn(data);
-    theList.appendChild(newListEntries);
-
+    var newListEntries = journalReturn(data.entries[i]);
+    theList.prepend(newListEntries);
   }
 });
